25dffe0b483989c003930ddfc1fdbbc830e6ebe9
fatehaliverdievv <tu7l7qsfn@code.edu.az>
2022-11-04 01:21:51 +0400

    created task file

---
+++ task4november/Program.cs
@@ -0,0 +1,30 @@
+﻿#region
+////TASK 1
+//using system.componentmodel.dataannotations;
+//int[] array = { 13, 8, 0, 2, 7, 1 };
+//int min = array[0];
+//int max = array[0];
+//int minindex = 0;
+//int maxindex = 0;
+//int temp = 0;
+//for (int i = 0; i < array.length; i++)
+//{
+//    if (array[i] < min)
+//    {
+//        min = array[i];
+//        minindex = i;
+//    }
+//    if (array[i] > max)
+//    {
+//        max = array[i];
+//        maxindex = i;
+//    }
+//}
+//temp = array[maxindex];
+//array[maxindex] = array[minindex];
+//array[minindex] = temp;
+//for (int i = 0; i < array.length; i++)
+//{
+//    console.writeline(array[i]);
+//}
+#endregion
